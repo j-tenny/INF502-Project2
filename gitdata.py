86c3693998614dd7bcc267208c7fe9bb9d53903d
Johnathan Tenny <107655580+j-tenny@users.noreply.github.com>
2023-11-22 15:22:55 -0700

    More pull request data + interface improvements (#16)
    * Interface improvements + more PullRequest data
    
    Pull requests now download diff and commit files to determine number of commits, additions, deletions, and file changes
    
    Created a get_github_api_request function with option for authentication token and more exception handling. Implemented for pull request api calls.
    
    Created and added an authentication token to main
    
    Improved user input validation and error handling for downloading repository data
    
    Added functionality to display list of pull requests
    
    * Fix git issues, minor format changes
    
    NOTE: CAN NOT include token in a commit, otherwise everything gets screwed up

--- gitdata.py
+++ gitdata.py
@@ -17,10 +17,11 @@ class GitHubUser:
 
 class Repository:
     # TODO: create data structures for github users and add functionality to summarize users who contributed to this repo
-    def __init__(self, owner_name, repo_name):
+    def __init__(self, owner_name, repo_name, token=None):
         # Assign properties
         self.owner_name = owner_name
         self.repo_name = repo_name
+        self.__token = token
 
         # Initialize an empty variable
         self.pull_requests = tuple()
@@ -29,26 +30,14 @@ class Repository:
         self.get_pulls()
 
     def get_pulls_as_json(self):
-        import requests
-
         # GitHub API endpoint for pull requests
         url = f"https://api.github.com/repos/{self.owner_name}/{self.repo_name}/pulls"
 
-        # Make a GET request to retrieve pull requests
-        response = requests.get(url)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Parse the JSON response
-            pull_requests_json = response.json()
-
-        else:
-            print(f"Failed to retrieve pull requests. Status code: {response.status_code}")
-            print(response.text)
+        pull_requests_json = get_github_api_request(url = url,convert_json=True,token=self.__token)
 
         return pull_requests_json
 
-    def get_pulls(self):
+    def get_pulls(self,token=None):
         # Get pull requests from github in json format
         pulls_json = self.get_pulls_as_json()
 
@@ -58,7 +47,7 @@ class Repository:
         # Convert each pull request in the json to a PullRequest object and add it
         # to the list of pull requests stored in this Repository object
         for json_record in pulls_json:
-            pull_request_instance = PullRequest()
+            pull_request_instance = PullRequest(token=self.__token)
             pull_request_instance.fill_from_json(json_record)
             pull_requests_list.append(pull_request_instance)
 
@@ -79,18 +68,22 @@ class Repository:
     def __repr__(self):
         return f'Repository(owner_name: {self.owner_name}, repo_name: {self.repo_name}, n_pull_requests: {len(self.pull_requests)})'
 class PullRequest:
-  def __init__(self,title:str = None, number:int = None, body:str = None, state:str = None, created_at:str = None, closed_at:str = None, user:str=None,  commits:str=None, additions:str=None, deletions:str=None, changed_files:str=None,):
+  def __init__(self,title:str = None, number:int = None, body:str = None, state:str = None, created_at:str = None, closed_at:str = None, user:str=None,  commits:str=None, additions:str=None, deletions:str=None, changed_files:str=None,token=None):
+
     self.title = title
     self.number = number
     self.body = body
     self.state = state
     self.created_at = created_at
-    self.closed_at = closed_at        
-    self.user = user  # User is an instance of GitHubUser
-    self.commits = commits
-    self.additions = additions
-    self.deletions = deletions
-    self.changed_files = changed_files
+    self.closed_at = closed_at
+    self.user = user
+    self.num_commits = commits
+    self.num_additions = additions
+    self.num_deletions = deletions
+    self.num_changed_files = changed_files
+
+    self.__token = token #Store token for making API requests. DO NOT INCLUDE IN OUTPUTS.
+
 
 
   def fill_from_json(self,json):
@@ -100,9 +93,9 @@ class PullRequest:
     self.state = json['state']
     self.created_at = json['created_at']
     self.closed_at = json['closed_at']
-    self.user = json['user']['login']  # User is an instance of GitHubUser
-    self.commits_url = json['commits_url']
-    self.diff_url = json['diff_url']
+    self.user = json['user']['login']
+    self.commits_url = json['commits_url'] # Don't need to output
+    self.diff_url = json['diff_url'] # Don't need to output
 
     self.get_num_commits()
     self.get_diff_metrics()
@@ -122,16 +115,71 @@ class PullRequest:
             }
 
   def get_num_commits(self):
-    # TODO get number of commits from commits_url
-    self.num_commits = int()
+    commits_json = get_github_api_request(url=self.commits_url,convert_json=True,token=self.__token)
+
+    self.num_commits = len(commits_json)
+
   def get_diff_metrics(self):
-    # TODO get number of additions, deletions, changed files from diff_url
-    self.num_additions = int()
-    self.num_deletions = int()
-    self.num_changed_files = int()
+    # Download diff data from API
+    diff_text = get_github_api_request(url = self.diff_url,convert_json=False,token=self.__token)
+
+    additions = 0
+    deletions = 0
+    changed_files = 0
+
+    # Count number of lines that start with "diff", "+" or "-"
+    for line in diff_text.split('\n'):
+        if len(line) > 0:
+            if line[0] == '+':
+                if line[0:3] == '+++':
+                    pass
+                else:
+                    additions += 1
+            elif line[0] == '-':
+                if line[0:3] == '---':
+                    pass
+                else:
+                    deletions += 1
+            elif line[0] == 'd':
+                if line[0:4] == 'diff':
+                    changed_files += 1
+
+    self.num_additions = additions
+    self.num_deletions = deletions
+    self.num_changed_files = changed_files
+
 
   def __str__(self):
     return f'Pull Request #{self.number}: {self.title}'
 
   def __repr__(self):
     return f'PullRequest(number:{self.number}, title:{self.title})'
+
+def get_github_api_request(url,convert_json=True, token=None):
+    import requests
+
+    if token is None:
+        # Make a GET request to retrieve pull requests
+        response = requests.get(url)
+    else:
+        # Headers including the Authorization token
+        headers = {"Authorization": f"token {token}"}
+
+        # Make a GET request to retrieve pull requests
+        response = requests.get(url, headers=headers)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        if convert_json:
+            return response.json()
+        else:
+            return response.text
+    elif response.status_code == 403:
+        raise PermissionError('Server Error 403: Access to Github API denied. Consider creating and using an'
+                              ' authentication token https://github.com/settings/tokens?type=beta. Read more: \n\n'+response.text)
+    elif response.status_code == 404:
+        raise ValueError('Error 404: No data found at this URL')
+    else:
+        raise ConnectionError(f"Failed to access Github API. Status code: {response.status_code} \n\n"+response.text)
+
