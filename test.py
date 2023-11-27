507d9eb5024c1150ecdbf1a85c2c16841172a3c8
vcsumedha <125511158+vcsumedha@users.noreply.github.com>
2023-11-27 11:16:00 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import gitdata
 
-jabref_repo = Repository('jabref', 'jabref')
+jabref_repo = gitdata.Repository('jabref', 'jabref',token = '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')
 n_users = jabref_repo.total_user()
 tot_pull_close = jabref_repo.total_pulls_closed()
-tot_pull_open = jabref_repo.total_pulls_opened()
+tot_pull_open = jabref_repo.total_pulls_open()
 old = jabref_repo.oldest()
 print(n_users)
-print(tot_pull_close)
 print(tot_pull_open)
+print(tot_pull_close)
 print(old)
