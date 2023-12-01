95310695253e9f867f56ac1fdf645daed32ff0aa
Johnathan Tenny <107655580+j-tenny@users.noreply.github.com>
2023-12-01 10:21:34 -0700

    Visualization updates, bug fixes (#32)
    * Interface improvements + more PullRequest data
    
    Pull requests now download diff and commit files to determine number of commits, additions, deletions, and file changes
    
    Created a get_github_api_request function with option for authentication token and more exception handling. Implemented for pull request api calls.
    
    Created and added an authentication token to main
    
    Improved user input validation and error handling for downloading repository data
    
    Added functionality to display list of pull requests
    
    * Fix git issues, minor format changes
    
    NOTE: CAN NOT include token in a commit, otherwise everything gets screwed up
    
    * Testing to_csv functions
    
    * Request "all" pull requests, added to examples.ipynb
    
    Realized that by default, the pulls api request only grabs "open" pull requests. Now it will grab "all" pull requests.
    
    * Added another error message for bad github credentials
    
    * Integration of summaries and csv/plot exports
    
    Also updated pull request api request to get "all" pull requests not just "open" pull requests (was the default).
    
    * Update gitdata.py
    
    updated the csv save to (hopefully) prevent delimiting when commas are present in the pull request.
    
    * Update .gitignore
    
    * Added export functionality and token input menu
    
    * Update gitdata.py
    
    Added visualization code pf boxplot and scatterplot
    
    * Update test.py
    
    * Get pulls in time window
    
    Previously we were only downloading first 30 pull requests. Now can download all pull requests within specified time window.
    
    Improved user experience + fixed bugs
    
    * CSV updates
    
    * Visualization functions to application
    
    * Added single repo visualizations to application
    
    * Update gitdata.py
    
    * Update examples, readme, requirements.txt
    
    * Visualization updates, bug fixes
    
    ---------
    
    Co-authored-by: pjdeasy86 <145591945+pjdeasy86@users.noreply.github.com>
    Co-authored-by: vcsumedha <125511158+vcsumedha@users.noreply.github.com>

--- application.py
+++ application.py
@@ -192,8 +192,7 @@ class GetRepoMenu:
 
         # Use these inputs to download data for a repo
         try:
-            repo_figures_path = self.app.repos_dir+owner_name+'-'+repo_name
-            repo_data = gitdata.Repository(owner_name,repo_name,time_window_days=time_window_days,token=self.app._token,output_path=repo_figures_path)
+            repo_data = gitdata.Repository(owner_name,repo_name,time_window_days=time_window_days,token=self.app._token,output_filepath=self.app.figures_dir)
         except KeyError as e:
             # If an exception occurs, start over
             print(str(e))
@@ -353,11 +352,17 @@ class RepoAnalysisMenu:
             print('Number of closed pull requests:'.rjust(44), repo.total_pulls_closed())
             print('Number of open pull requests:'.rjust(44), repo.total_pulls_open())
             print('Date opened for oldest pull request:'.rjust(44), repo.oldest())
-            print(repo.pull_request_correlations())
-            repo.box_closed_open_commit()
-            repo.box_addition_deletion()
-            repo.scatter_addition_deletion()
-            repo.file_changes_per_user()
+
+            print()
+            if len(repo.pull_requests) > 0:
+                print("Pull Request Correlation Matrix:")
+                print(repo.pull_request_correlations())
+                repo.box_closed_open_commit()
+                repo.box_addition_deletion()
+                repo.scatter_addition_deletion()
+                repo.file_changes_per_user()
+                print()
+                print('Summary figures saved to',os.path.abspath(self.app.figures_dir+f'repo_summary_{repo.repo_name}'))
 
             self.display()
 
